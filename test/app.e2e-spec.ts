1c6bb1b0b7086ca827c6fac59a86019706af59a4
tareknatsheh <tarek.o.natsheh@gmail.com>
2025-01-25 02:56:46 +0200

    e2e tests for user auth register and login

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,14 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import * as pactum from 'pactum';
+import { AuthDto, LoginDto } from '../src/auth/dto/auth.dto';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('smoke test', () => {
     let app: INestApplication;
+    let prisma: PrismaService;
+
     beforeAll(async () => {
         const moduleRef = await Test.createTestingModule({
             imports: [AppModule],
@@ -16,11 +21,71 @@ describe('smoke test', () => {
             }),
         );
         await app.init();
+        await app.listen(3333);
+        prisma = app.get(PrismaService);
+        pactum.request.setBaseUrl('http://localhost:3333');
+        await prisma.cleanDb();
     });
 
-    afterAll(() => {
+    afterAll(async () => {
         app.close();
     });
 
-    it.todo('should pass');
+    const dto: AuthDto = {
+        email: 'test@gmail.com',
+        password: '123123',
+        name: 'john',
+    };
+
+    describe('Auth', () => {
+        describe('register', () => {
+            it('should register new user', async () => {
+                return pactum
+                    .spec()
+                    .post('/auth/register')
+                    .withBody(dto)
+                    .expectStatus(HttpStatus.CREATED);
+            });
+            it('should throw an error if duplicate user email', async () => {
+                return pactum
+                    .spec()
+                    .post('/auth/register')
+                    .withBody(dto)
+                    .expectStatus(HttpStatus.FORBIDDEN)
+                    .expectBodyContains('Email already exist');
+            });
+            it('should throw an error if email is empty', async () => {
+                return pactum
+                    .spec()
+                    .post('/auth/register')
+                    .withBody({ password: '123' })
+                    .expectStatus(HttpStatus.BAD_REQUEST)
+                    .expectBodyContains('email should not be empty');
+            });
+        });
+        describe('login', () => {
+            it('should login', async () => {
+                return pactum
+                    .spec()
+                    .post('/auth/login')
+                    .withBody(dto)
+                    .expectStatus(HttpStatus.OK)
+                    .stores('userToken', 'access_token');
+            });
+        });
+    });
+    describe('User', () => {
+        it('should get /me', async () => {
+            return pactum
+                .spec()
+                .get('/users/me')
+                .withBearerToken('$S{userToken}')
+                .expectStatus(HttpStatus.OK);
+        });
+    });
+    describe('Bookmarks', () => {
+        it.todo('should create bookmark');
+        it.todo('should edit bookmark');
+        it.todo('should delete bookmark');
+    });
 });
